9f43f6a0445684323b3a6d7ddede799802daaaf3
sh167603 <soohwan06@gmail.com>
2025-02-20 11:41:47 +0900

    mai과  requirements 수정

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from api.concert_api import router as concert_router
 from api.reservation_api import router as reservation_router
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 
 app = FastAPI()
@@ -18,6 +19,8 @@ async def read_root():
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8003, reload=True)
