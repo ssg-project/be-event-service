d137f80f75a7e17513a15f5fa89e39e8209919ca
sh167603 <soohwan06@gmail.com>
2025-02-20 11:52:49 +0900

    test

--- utils/database.py
+++ utils/database.py
@@ -1,9 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DATABASE_URL = f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASS']}@{os.environ['DB_HOST']}/{os.environ['DB_NAME']}"
 
