dd148502183fc0c2a5827a79fb940294af4eeca4
kuni4210 <kuni4210@naver.com>
2025-02-21 11:43:37 +0900

    rollback

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from api.concert_api import router as concert_router
 from api.reservation_api import router as reservation_router
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware.cors import CORSMiddleware
-from prometheus_fastapi_instrumentator import Instrumentator
+# from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 
 app = FastAPI()
@@ -20,7 +20,7 @@ async def read_root():
 async def health_check():
     return {"status": "ok"}
 
-Instrumentator().instrument(app).expose(app)
+# Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8003, reload=True)
