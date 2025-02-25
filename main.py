83e021f94b39a9ab73d80f7ae9e28c85c9ad346a
sh167603 <soohwan06@gmail.com>
2025-02-25 11:04:19 +0900

    주석 제거, from 구문 삭제

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from api.concert_api import router as concert_router
 from api.reservation_api import router as reservation_router
-from starlette.middleware.sessions import SessionMiddleware
-from starlette.middleware.cors import CORSMiddleware
-# from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 
 app = FastAPI()
@@ -20,7 +18,7 @@ async def read_root():
 async def health_check():
     return {"status": "ok"}
 
-# Instrumentator().instrument(app).expose(app)
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8003, reload=True)
