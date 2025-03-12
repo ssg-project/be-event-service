bcb57e0e35a34e2b0a15d8bcdabb6258e51b0794
myeongsoyi <myeongsoyi@gmail.com>
2025-03-12 16:25:56 +0900

    feat: config 수정

--- config/config.py
+++ config/config.py
@@ -1,13 +1,60 @@
 import os
 from dotenv import load_dotenv
+import boto3
+import json
+from botocore.exceptions import ClientError
 
 if not os.getenv("APP_ENV"):
     load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
+    REDIS_HOST = os.getenv("REDIS_HOST")
+    REDIS_PORT = os.getenv("REDIS_PORT")
 
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = os.getenv("REDIS_PORT")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_NAME = os.getenv("DB_NAME")
+
+else:
+    secret_name = "secret/ticketing/event"
+    region_name = "ap-northeast-2"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    
+    secret_data = json.loads(secret)
+
+    REDIS_HOST = secret_data["REDIS_HOST"]
+    REDIS_PORT = secret_data["REDIS_PORT"]
+
+    db_secret_name = "secret/ticketing/rds"
+    
+    try:
+        db_secret_response = client.get_secret_value(
+            SecretId=db_secret_name
+        )
+    except ClientError as e:
+        raise e
+    
+    db_secret = db_secret_response['SecretString']
+    db_secret_data = json.loads(db_secret)
+    
+    # DB 관련 정보 설정
+    DB_USER = db_secret_data["username"]
+    DB_PASS = db_secret_data["password"]
+    DB_HOST = db_secret_data["host"]
+    DB_PORT = db_secret_data["port"]
+    DB_NAME = db_secret_data["dbInstanceIdentifier"]
